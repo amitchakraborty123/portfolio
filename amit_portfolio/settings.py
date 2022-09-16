3be116d5f1a5d291d75324832ea8160f875dc00b
Amit Chakraborty <akchakraborty55@gmail.com>
2022-09-16 23:18:46 +0600

    Fifth commit

--- amit_portfolio/settings.py
+++ amit_portfolio/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
+# import django_heroku
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,7 +128,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 # Activate Django-heroku
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
