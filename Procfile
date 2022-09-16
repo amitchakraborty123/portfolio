e5863ebd9274b4e660f8c45b07a0cb4e011b1a07
Amit Chakraborty <akchakraborty55@gmail.com>
2022-09-16 18:36:58 +0600

    Second commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn myproject.wsgi
+web gunicorn amit_portfolio.wsgi:application --log-file -
--- amit_portfolio/settings.py
+++ amit_portfolio/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import django_heroku
+
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,12 +121,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 # Activate Django-heroku
 django_heroku.settings(locals())
